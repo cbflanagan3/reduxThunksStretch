d1e7de56f10352f18cdb74ee0517fd5714a2f9f5
Caroline B Flanagan <carolineflan@gmail.com>
2020-07-08 18:57:35 -0400

    reduxthunks

--- actions.js
+++ actions.js
@@ -3,5 +3,34 @@ const axios = require('axios');
 //TYPES
 const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
 
+const gotDesserts = () => {
+        store.dispatch(() => {
+          return axios.get('/api/desserts')
+            .then((res) => {
+              const {data: {desserts}} = res;
+              dispatch({
+                type: GOT_DESSERTS_DATA,
+                desserts,
+              })
+            
+            })
+            })
+      }
+}
+
+const addDessert = function(dispatch) {
+    return axios.post('/api/desserts')
+        .then(({data}) => {
+            dispatch(newDessertData(data))
+        })
+}
+
+const fetchDesserts = async() => {
+    store.dispatch({type: 'GOT_DESSERTS_DATA', desserts: (await axios.get('/api/desserts')).data})
+}
+
+const postDessert = function(dispatch) {
+    return null;
+}
 /* YOUR CODE GOES HERE */
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
